85323976c74f9499a5a80138db56daccdacdcdc7
akadiraslan <akadirasl4n@gmail.com>
2021-02-09 13:46:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-h8zz7r
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-h8zz7r)
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+      </div>
+    );
+  }
+}
+
+export default App;
